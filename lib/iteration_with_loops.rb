e96d47131320edfe737f65694bedd9c37521a0e0
Paul Ralyea <047par@gmail.com>
2020-05-07 18:44:55 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,7 +5,11 @@ while row_index < src.count do
   element_index = 0
   longest_string_element = ""
   while element_index < src[row_index].count do
-  
+ if src[row_index][element_index].length > longest_string_element.length
+      longest_string_element = src[row_index][element_index]
+    end
+    element_index += 1
+  end 
   
   
   # src will be an array of arrays of integers
